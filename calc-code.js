60bbb06012f32751033ad4845f8ad252d27ae8da
Noah Howell <nhowell117@gmail.com>
2022-02-02 11:43:30 -0500

    Cleaned up CSS

--- calc-code.js
+++ calc-code.js
@@ -1,5 +1,5 @@
 function add(x, y) {
-    return x + y;
+    return +x + +y;
 }
 
 function subtract(x, y) {
@@ -11,7 +11,14 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
-    return x / y;
+    if (y == 0) {
+        alert("You can't break my program, no dividing by 0 in my domain!");
+        clearCalc();
+        return;
+    }
+    else {
+        return x / y;
+    }
 }
 
 function operate(operator, num1, num2) {
@@ -28,3 +35,63 @@ function operate(operator, num1, num2) {
         return divide(num1, num2);
     }
 }
+
+function displayValue(num) {
+    if (num === ".") {
+        decimalButton.disabled = true;
+    }
+    if (numberDisplayed.textContent ===  "+" || numberDisplayed.textContent ===  "-" || numberDisplayed.textContent ===  "*" || numberDisplayed.textContent ===  "/") {
+        numberDisplayed.textContent = "";
+    }
+    numberDisplayed.textContent += num;
+}
+
+function operatorPressed(operator) {
+    decimalButton.disabled = false;
+    operatorInput.push(operator);
+    numberInput.push(numberDisplayed.textContent);
+    numberDisplayed.textContent = operator;
+}
+
+function equalsPressed() {
+    numberInput.push(numberDisplayed.textContent);
+    if (operatorInput.length === 0) {
+        let placeholder = numberDisplayed.textContent;
+        clearCalc();
+        numberDisplayed.textContent = placeholder;
+        return;
+    }
+    let finalValue = Math.round((operate(operatorInput[0], numberInput[0], numberInput[1])) * 1000) / 1000;
+    operatorInput.shift();
+    numberInput.shift();
+    numberInput.shift();
+    if (operatorInput.length !== 0) {
+        while(operatorInput.length !== 0) {
+            finalValue = operate(operatorInput[0], finalValue, numberInput[0]);
+            operatorInput.shift();
+            numberInput.shift();
+        }
+    }
+    clearCalc();
+    numberDisplayed.textContent = finalValue;
+    decimalButton.disabled = false;
+}
+
+function clearCalc() {
+    while(numberInput.length > 0) {
+        numberInput.pop();
+    }
+    while(operatorInput.length > 0) {
+        operatorInput.pop();
+    }
+    numberDisplayed.textContent = "";
+}
+
+function backspace() {
+    numberDisplayed.textContent = numberDisplayed.textContent.slice(0, -1);
+}
+
+const numberDisplayed = document.querySelector('.display');
+const decimalButton = document.querySelector('.decimal-button');
+const numberInput = [];
+const operatorInput = [];
